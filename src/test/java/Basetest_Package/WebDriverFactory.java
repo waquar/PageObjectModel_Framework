789ba6f7f33f489872db03f9813432f650d3530b
waquar <waquar.alam92@gmail.com>
2020-01-23 12:12:07 +0530

    added drivers properties and optimized custom driver

--- src/test/java/Basetest_Package/WebDriverFactory.java
+++ src/test/java/Basetest_Package/WebDriverFactory.java
@@ -22,6 +22,7 @@ public class WebDriverFactory {
     private static ThreadLocal<WebDriver> threadedDriver = new ThreadLocal<WebDriver>();
     public WebDriver getDriver(String browser) {
         WebDriver driver = null;
+        setDriver(browser);     //seeting properties here
 
         if (threadedDriver.get() == null ){
             try {
@@ -45,5 +46,34 @@ public class WebDriverFactory {
         }
         return threadedDriver.get();
     }
+    //setting system properties according to machine and browser
+    private void setDriver(String browser) {
+        String driverPath = "";
+        String os = System.getProperty("os.name").toLowerCase().substring(0, 3);
+        System.out.println("OS Name from system property :: " + os);
+        String directory = System.getProperty("user.dir") + "//drivers//";
+        String driverKey = "";
+        String driverValue = "";
+
+        if (browser.equalsIgnoreCase("firefox")) {
+            driverKey = "webdriver.gecko.driver";
+            driverValue = "geckodriver";
+        } else if (browser.equalsIgnoreCase("chrome")) {
+            driverKey = "webdriver.chrome.driver";
+            driverValue = "chromedriver";
+        } else if (browser.equalsIgnoreCase("ie")) {
+            driverKey = "webdriver.ie.driver";
+            driverValue = "IEDriverServer";
+        } else {
+            System.out.println("Browser type not supported");
+        }
+
+        //if machine is windows then it must be .exe exetension for mac or linux not required
+        //used ternary operator for that
+        driverPath = directory + driverValue + (os.equals("win") ? ".exe" : "");
+        System.out.println("Driver Binary :: " + driverPath);
+        System.setProperty(driverKey, driverPath);
+    }
+
 
 }
