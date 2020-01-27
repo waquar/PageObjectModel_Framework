e639e01b317ba360b900a9d71e8bb3555a23b711
waquar <waquar.alam92@gmail.com>
2020-01-27 12:28:59 +0530

    started data driven implementation

---
+++ src/test/java/com/overview/Excel_Read.java
@@ -0,0 +1,35 @@
+package com.overview;
+
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class Excel_Read {
+   public static void main(String[] args){
+       XSSFWorkbook excelworkbook;
+       XSSFSheet excelsheet;
+       XSSFCell excelcell;
+
+       String path = "//Users//waquaralam//Framework_POM//src/main//resources//Electronic Wastage.xlsx";
+       String sheetname = "Ewaste";
+
+       try{
+           FileInputStream fs = new FileInputStream(path);
+           excelworkbook = new XSSFWorkbook(fs);
+           excelsheet = excelworkbook.getSheet(sheetname);
+           excelcell = excelsheet.getRow(5).getCell(0);
+
+           String dataincell = excelcell.getStringCellValue();
+           System.out.println(dataincell);
+
+       } catch (IOException e) {
+           e.printStackTrace();
+       }
+
+
+   }
+
+}
